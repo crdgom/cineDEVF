946c3dbebb8e74fe775a060f29ca51c1267c8fdf
carlos <crdgom@gmail.com>
2023-06-20 13:47:54 -0600

    Complex CRUD Completed

--- backend/src/controllers/cinemaComplexController.js
+++ backend/src/controllers/cinemaComplexController.js
@@ -48,3 +48,73 @@ export const createCinemaComplex = async (req, res) =>{
         res.status(409).json({message: error.message});
     }
 }
+
+export const updateCinemaComplex = async (req, res) => {
+    const { id } = req.query;
+
+    try {
+        if (!id) {
+            return res.status(400).json({ message: "Missing cinema complex id" });
+        }
+
+        const cinemaComplex = await CinemaComplex.findById(id);
+        if (!cinemaComplex) {
+            return res.status(404).json({ message: "Cinema complex not found" });
+        }
+
+        // Actualizar solo los parámetros que se recibieron en la query
+        if (req.query.complex_name) {
+            cinemaComplex.complex_name = req.query.complex_name;
+        }
+        if (req.query.complex_number) {
+            cinemaComplex.complex_number = req.query.complex_number;
+        }
+        if (req.query.auditoriums) {
+            cinemaComplex.auditoriums = req.query.auditoriums;
+        }
+        if (req.query.street) {
+            cinemaComplex.address.street = req.query.street;
+        }
+        if (req.query.exterior_number) {
+            cinemaComplex.address.exterior_number = req.query.exterior_number;
+        }
+        if (req.query.postal_code) {
+            cinemaComplex.address.postal_code = req.query.postal_code;
+        }
+        if (req.query.city) {
+            cinemaComplex.address.city = req.query.city;
+        }
+        if (req.query.district) {
+            cinemaComplex.address.district = req.query.district;
+        }
+        if (req.query.zone) {
+            cinemaComplex.address.zone = req.query.zone;
+        }
+
+        const updatedCinemaComplex = await cinemaComplex.save();
+        res.status(200).json(updatedCinemaComplex);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteCinemaComplex = async (req, res) => {
+    const { id } = req.query;
+
+    try {
+        if (!id) {
+            return res.status(400).json({ message: "Missing cinema complex id" });
+        }
+
+        const cinemaComplex = await CinemaComplex.findById(id);
+        if (!cinemaComplex) {
+            return res.status(404).json({ message: "Cinema complex not found" });
+        }
+
+        await cinemaComplex.deleteOne({_id: id});
+        res.status(200).json({ message: "Cinema complex deleted successfully" });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- backend/src/routes/cinemaComplexRoutes.js
+++ backend/src/routes/cinemaComplexRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getCinemaComplexes, getCinemaComplex, createCinemaComplex } from '../controllers/cinemaComplexController.js';
+import { getCinemaComplexes, getCinemaComplex, createCinemaComplex, updateCinemaComplex, deleteCinemaComplex } from '../controllers/cinemaComplexController.js';
 
 const router = Router();
 
@@ -8,7 +8,7 @@ const router = Router();
 router.get('/api/v1/getComplexes', getCinemaComplexes);
 router.get('/api/v1/getComplex', getCinemaComplex);
 router.post('/api/v1/createComplex', createCinemaComplex);
-router.put('/api/v1/updateComplex');
-router.delete('/api/v1/deleteComplex');
+router.put('/api/v1/updateComplex', updateCinemaComplex);
+router.delete('/api/v1/deleteComplex', deleteCinemaComplex);
 
 export default router;
