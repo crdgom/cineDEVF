81ac15017924f56e1f941d85ecf11573e1397e2f
carlos <crdgom@gmail.com>
2023-06-20 11:00:55 -0600

    Create Cinema Complex by URL Params controller & route

--- backend/src/controllers/cinemaComplexController.js
+++ backend/src/controllers/cinemaComplexController.js
@@ -8,3 +8,43 @@ export const getCinemaComplexes = async (req, res) => {
         res.status(404).json({message: error.message});
     }
 }
+
+export const getCinemaComplex = async (req, res) => {
+    const { id } = req.query;
+    try{
+        if(!id){
+            res.status(400).json({message: "Missing cinema complex id"});
+        }else{
+            const cinemaComplex = await CinemaComplex.findById(id);
+        res.status(200).json(cinemaComplex);
+        }
+        
+    }catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createCinemaComplex = async (req, res) =>{
+    const { complex_name, complex_number, auditoriums, street, exterior_number, postal_code, city, district, zone} = req.query;
+
+    try{
+        const newCinemaComplex = new CinemaComplex({
+            complex_name,
+            complex_number,
+            auditoriums,
+            address:{
+                street,
+                exterior_number,
+                postal_code,
+                city,
+                district,
+                zone
+            }
+        });
+        await newCinemaComplex.save();
+        res.status(201).json(newCinemaComplex);
+    }
+    catch(error){
+        res.status(409).json({message: error.message});
+    }
+}
--- backend/src/routes/cinemaComplexRoutes.js
+++ backend/src/routes/cinemaComplexRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import { getCinemaComplexes } from '../controllers/cinemaComplexController.js';
+import { getCinemaComplexes, getCinemaComplex, createCinemaComplex } from '../controllers/cinemaComplexController.js';
 
 const router = Router();
 
 // * Endpoints for cinema complex (CRUD) operations (getCinemaComplexes, getCinemaComplex, createCinemaComplex, updateCinemaComplex, deleteCinemaComplex)
 
 router.get('/api/v1/getComplexes', getCinemaComplexes);
-router.get('/api/v1/getComplex');
-router.post('/api/v1/createComplex');
+router.get('/api/v1/getComplex', getCinemaComplex);
+router.post('/api/v1/createComplex', createCinemaComplex);
 router.put('/api/v1/updateComplex');
 router.delete('/api/v1/deleteComplex');
 
